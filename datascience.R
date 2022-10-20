4a1ba55ca86bc70eafeadf6cbbbe25292913fa20
Obinna Anthony Onyekwere <98945808+xyoung7123@users.noreply.github.com>
2022-10-20 15:03:09 +0100

    Creating Rstudio Code
    additional r code used for installing libraries and notebook preparation

---
+++ datascience.R
@@ -0,0 +1,22 @@
+## Installing packages
+install.packages("tidyverse")
+install.packages("lattice")
+install.packages("janitor")
+install.packages("lubridate")
+install.packages("ggplot2")
+install.packages("dplyr")
+install.packages("plotly")
+install.packages("leaflet")
+
+
+## loading up on libraries
+library(tidyverse)
+library(lattice)
+library(janitor)
+library(lubridate)
+library(dplyr)
+library(ggplot2)
+library(plotly)
+library(leaflet)
+library(markdown)
+library(rmarkdown)
